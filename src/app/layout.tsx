396406504c7200290f6739def5beaa679735ba58
Rachel Tomasetti <build.ray.build@gmail.com>
2025-11-23 17:11:39 -0500

    updated layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { ModalProvider } from "./contexts/ModalContext";
 import { poppins } from "./fonts";
 
 export const metadata: Metadata = {
-  title: "This is Ki",
+  title: "Welcome to Ki",
   description: "mind garden",
   viewport: {
     width: "device-width",

d395a1d9e538617edb24c26219ae231b666bad54
raybuilds021 <racheltomasetti9@gmail.com>
2026-09-21 03:12:37 -0400

    take 2

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,7 +5,9 @@ export default defineCliConfig({
     projectId: 'trxlasoy',
     dataset: 'production',
   },
+  studioHost: 'racheltomasetti',
   deployment: {
+    appId: 'ki9ulb0v19ogish0x1h59fkw',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
